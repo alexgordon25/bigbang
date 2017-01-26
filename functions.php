afb95dc792679f482d84c5ea0272f7882028036b
dcaste <daxc@rain.agency>
2017-01-26 09:35:37 -0600

    Reorder funtions.

--- functions.php
+++ functions.php
@@ -7,6 +7,15 @@
  * @package bigbang
  */
 
+/**
+ * This is a list of theme functionalities.
+ * If you don't want to use any of these, just comment the line
+ * or you can modifiy every file and enable/disable any functionality.
+ */
+
+// Theme support options
+require_once(get_template_directory().'/functions/theme-support.php'); 
+
 if ( ! function_exists( 'bigbang_setup' ) ) :
 /**
  * Sets up theme defaults and registers support for various WordPress features.
@@ -27,13 +36,7 @@ function bigbang_setup() {
 	// Add default posts and comments RSS feed links to head.
 	add_theme_support( 'automatic-feed-links' );
 
-	/*
-	 * Let WordPress manage the document title.
-	 * By adding theme support, we declare that this theme does not use a
-	 * hard-coded <title> tag in the document head, and expect WordPress to
-	 * provide it for us.
-	 */
-	add_theme_support( 'title-tag' );
+	
 
 	/*
 	 * Enable support for Post Thumbnails on posts and pages.
